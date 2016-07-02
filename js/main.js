a682651669362935ab8585c38aea39f07abaf355
Dolmat <anton.dolmat@gmail.com>
2016-07-02 15:35:39 +0300

    socials and order

--- js/main.js
+++ js/main.js
@@ -50,5 +50,54 @@ function closeToast() {
 var $contactUsMessage = $('<div><h5>Уважаемые коллеги!</h5> <p>Ваша заявка получена, и принята в работу. В ближайшее время (не позднее 24 часов Вы получите ответ или готовое коммерческое предложение. В случае если оно Вас устроит, Вам будет выставлен официальный счет для оплаты.</p><p> С уважением коллектив ООО НДА Деловая медицинская компания</p><div><a href="#!" onclick="closeToast()" class=" modal-action modal-close waves-effect waves-blue btn-flat">Закрыть</a></div></div>');
 
 function contactUsShowMessage() {
+  $('#contact-us-modal').closeModal();
   Materialize.toast($contactUsMessage, 20000, 'toast-style grey lighten-5');
 }
+
+function newOrderShowMessage() {
+  $('#modal-add-order').closeModal();
+  Materialize.toast($contactUsMessage, 20000, 'toast-style grey lighten-5');
+}
+
+function contactUsTopShowMessage() {
+  $('#contactsModal').closeModal();
+  Materialize.toast($contactUsMessage, 20000, 'toast-style grey lighten-5');
+}
+
+// Validating forms
+
+//Validating email in order modal
+
+var email = document.getElementById("email-order");
+
+email.addEventListener("keyup", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Введите Ваш email");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+// Validate email in contactUsTop
+
+var email = document.getElementById("email");
+
+email.addEventListener("keyup", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Введите Ваш email");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+// Validate email bottom
+
+var email = document.getElementById("email-bottom");
+
+email.addEventListener("keyup", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Введите Ваш email");
+  } else {
+    email.setCustomValidity("");
+  }
+});
